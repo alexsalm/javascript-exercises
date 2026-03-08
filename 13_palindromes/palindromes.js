53c489eb14b75f3ce4f80de5d70b3b233014cd62
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-03-08 11:58:14 -0700

    Implement palindromes function (this is a direct copy of solution file

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,7 +1,15 @@
 const palindromes = function (string) {
-    let newArray = string.slice(0, string.size() - 1);
-    newArray.reverse();
-    return newArray;
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanedString = string
+        .toLowerCase()
+        .split()
+        .filter((character) => alphanumerical.includes(character))
+        .join();
+
+    const reversedString = cleanedString.split('').reverse().join('');
+
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
