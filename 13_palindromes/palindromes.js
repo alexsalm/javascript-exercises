896e9956615b15d0906ccd51de39a2d888adf4c7
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-03-08 11:02:40 -0700

    Implement palindromes function, to be tested

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newArray = string.slice(0, string.size() - 1);
+    newArray.reverse();
+    return newArray;
 };
 
 // Do not edit below this line
