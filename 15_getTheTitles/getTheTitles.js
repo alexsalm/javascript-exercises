b333d30d0c2b0937a983a2d445f741489ceeebca
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-04-06 10:22:15 -0700

    Add code from js-exercises directory

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const books = [
+    {
+        title: "Book",
+        author: "Name"
+    },
+    {
+        title: "Book2",
+        author: "Name2"
+    }
+]
 
+const getTheTitles = function(array) {
+    const bookTitles = array.map((element) => element.title);
+    return bookTitles;
 };
 
 // Do not edit below this line
